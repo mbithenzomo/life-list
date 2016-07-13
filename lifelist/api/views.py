07e0a6f0c4accc2a18fcec137e8bd7e3904d9389
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-07-13 10:13:29 +0300

    [Feature] Add views to view all bucket lists, and to add, view, update, and delete individual bucket lists

--- lifelist/api/views.py
+++ lifelist/api/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from api.models import Bucketlist
+from api.serializers import BucketlistSerializer
 
-# Create your views here.
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs["content_type"] = "application/json"
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def bucketlist_list(request):
+    """
+    List all bucket lists, or create a new bucket list.
+    """
+    if request.method == 'GET':
+        bucketlists = Bucketlist.objects.all()
+        serializer = BucketlistSerializer(bucketlists, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BucketlistSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def bucketlist_detail(request, pk):
+    """
+    Retrieve, update or delete a bucket list.
+    """
+    try:
+        bucketlist = Bucketlist.objects.get(pk=pk)
+    except Bucketlist.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = BucketlistSerializer(bucketlist)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = BucketlistSerializer(bucketlist, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        bucketlist.delete()
+        return HttpResponse(status=204)
