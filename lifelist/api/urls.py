8f542a4a3393a5343fc54caf93d455066a2032ba
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-12 18:51:31 +0300

    [Feature] Refactor urls.py to use routers

--- lifelist/api/urls.py
+++ lifelist/api/urls.py
@@ -1,32 +1,13 @@
 from django.conf.urls import url, include
-from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views as authviews
-from api.views import IndexView, HomeView, BucketlistViewSet, \
-                      UserViewSet, TestView
 from api import views
 
-bucketlists = BucketlistViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-bucketlist = BucketlistViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-users = UserViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-user = UserViewSet.as_view({
-    'get': 'retrieve'
-})
+router = DefaultRouter()
+router.register(r'bucketlists', views.BucketlistViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    url(r'^bucketlists/$', bucketlists, name='bucketlists'),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/$', bucketlist, name='bucketlist'),
-    url(r'^users/$', users, name='users'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user, name='user'),
+    url(r'^', include(router.urls)),
     url(r'^auth/login/', authviews.obtain_auth_token),
 ]
