cd375968a8dc0c186c4a8f6926f64ff8bcd98a64
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-15 21:42:38 +0300

    [Feature] Complete tests for operations on bucket lists

--- lifelist/api/tests/test_api.py
+++ lifelist/api/tests/test_api.py
@@ -26,21 +26,21 @@ class TestBase(APITestCase):
                      "password": "testpassword"}
         self.client.post(url, data=self.user)
 
-        # Add test bucket list
+        # Add first test bucket list
         url = reverse("bucketlist-list")
         self.bucketlist = {"title": "The List of Awesome",
                            "description": "Awesome things!",
                            "created_by": 1}
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.get_token())
-        self.client.post(url, data=self.bucketlist)
+        response = self.client.post(url, data=self.bucketlist)
+        self.first_bucketlist_id = str(response.data["id"])
 
-        # Add test bucket list item
-        # url = reverse("bucketlist-items-list")
-        # self.item = {"title": "Swim with dolphins",
-        #              "description": "Swim with dolphins in Watamu",
-        #              "bucketlist_id": 1,
-        #              "created_by": 1}
-        # self.client.post(url, data=self.item)
+        # Add second test bucket list
+        self.bucketlist = {"title": "Knowledge Goals",
+                           "description": "Things to learn",
+                           "created_by": 1}
+        response = self.client.post(url, data=self.bucketlist)
+        self.second_bucketlist_id = str(response.data["id"])
 
 
 class TestAuth(TestBase):
@@ -117,21 +117,101 @@ class TestBucketlists(TestBase):
         self.assertTrue("Invalid token" in response.data["detail"])
 
     def test_add_bucketlist(self):
-        """ Test that user cannot add a bucket list """
+        """ Test that user can add a bucket list """
         url = reverse("bucketlist-list")
-        self.bucketlist = {"title": "Knowledge Goals",
-                           "description": "Things to learn",
+        self.bucketlist = {"title": "Adventure!",
+                           "description": "Adventurous stuff",
                            "created_by": 1}
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.get_token())
         response = self.client.post(url, data=self.bucketlist)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Bucketlist.objects.count(), 3)
+        self.assertTrue("Adventure!" in response.data["title"])
+        self.assertTrue("Adventurous stuff" in response.data["description"])
+
+    def test_delete_bucketlist(self):
+        """ Test deletion of bucket lists """
+        url = "/api/v1/bucketlists/" + self.first_bucketlist_id + "/"
+        response = self.client.delete(url)
+        # Only one bucket list remains
+        self.assertEqual(Bucketlist.objects.count(), 1)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_edit_bucketlist(self):
+        """ Test editing of bucket lists """
+        self.bucketlist = {"title": "Mission Multilinguist",
+                           "description": "Languages to learn"}
+        url = "/api/v1/bucketlists/" + self.first_bucketlist_id + "/"
+        response = self.client.put(url, data=self.bucketlist)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue("Mission Multilinguist" in response.data["title"])
+        self.assertTrue("Languages to learn" in response.data["description"])
+
+    def test_get_bucketlists(self):
+        """ Test that all bucket lists are displayed """
+        url = reverse("bucketlist-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        bucketlist1 = response.data[0]
+        bucketlist2 = response.data[1]
+        # Both bucket lists are displayed
+        self.assertEqual(bucketlist1.get("title"), "The List of Awesome")
+        self.assertEqual(bucketlist2.get("title"), "Knowledge Goals")
+
+    def test_get_bucketlist(self):
+        """ Test that specified bucket list is displayed """
+        # Get first bucket list
+        url = "/api/v1/bucketlists/" + self.first_bucketlist_id + "/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("title"), "The List of Awesome")
+
+        # Get second bucket list
+        url = "/api/v1/bucketlists/" + self.second_bucketlist_id + "/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("title"), "Knowledge Goals")
+
+    def test_get_nonexistent_bucketlist(self):
+        """
+        Test that specifying a bucket list with invalid id
+        will throw an error
+        """
+        url = "/api/v1/bucketlists/1234/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue("Not found" in response.data["detail"])
+
+    def test_unauthorized_access(self):
+        """
+        Test that users cannot edit or delete another user's bucket lists
+        """
+        # Register a new user
+        url = reverse("user-list")
+        self.user = {"username": "testuser2",
+                     "email": "testuser2@email.com",
+                     "password": "testpassword"}
+        self.client.post(url, data=self.user)
+
+        # Log new user in and obtain their token
+        url = reverse("login")
+        self.user = {"username": "testuser2",
+                     "password": "testpassword"}
+        response = self.client.post(url, data=self.user)
+        token = str(response.data.get("token"))
+
+        # Cannot edit bucket list
+        self.bucketlist = {"title": "Mission Multilinguist",
+                           "description": "Languages to learn"}
+        url = "/api/v1/bucketlists/" + self.first_bucketlist_id + "/"
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token)
+        response = self.client.put(url, data=self.bucketlist)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue("You do not have permission to perform this action"
+                        in response.data["detail"])
+
+        # Cannot delete bucket list
+        url = "/api/v1/bucketlists/" + self.first_bucketlist_id + "/"
+        response = self.client.delete(url)
+        # Number of bucket lists remains the same
         self.assertEqual(Bucketlist.objects.count(), 2)
-        self.assertTrue("Knowledge Goals" in response.data["title"])
-        self.assertTrue("Things to learn" in response.data["description"])
-
-
-class TestItems(TestBase):
-    """ Test operations on bucket list items"""
-
-    def setUp(self):
-        pass
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
