8f9209971506237be7c29edb9f74f4be086254ff
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-16 12:32:56 +0300

    [Fix] Add 'django.contrib.sites' to installed apps setting

--- lifelist/lifelist/settings/base.py
+++ lifelist/lifelist/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     # Third Party Apps
     'rest_framework',
     'rest_framework.authtoken',
