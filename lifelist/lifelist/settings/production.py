5f10198a433c83f9cdd5edab700fb051aab325d8
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-25 09:56:43 +0300

    [Fix] Modify files for deployment

--- lifelist/lifelist/settings/production.py
+++ lifelist/lifelist/settings/production.py
@@ -1,11 +1,24 @@
 """
-Development specific settings for lifelist project.
+Production specific settings for lifelist project.
 """
 
 from .base import *
+import dj_database_url
 
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases# Update database configuration with $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
