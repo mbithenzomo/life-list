f26bb4a5cff01df97340864f6e498accb002eb52
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-08-25 11:00:42 +0300

    [Fix] Modify static files settings for production

--- lifelist/lifelist/settings/production.py
+++ lifelist/lifelist/settings/production.py
@@ -22,16 +22,3 @@ ALLOWED_HOSTS = ['*']
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
